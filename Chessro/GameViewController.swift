6fba928233113a51cb04b25571122db63b943b8e
Chen Z <chenzhu@ucsb.edu>
2020-05-18 11:46:09 -0700

    Initial Commit

---
+++ Chessro/GameViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GameViewController.swift
+//  Chessro
+//
+//  Created by Stone Zhu on 5/18/20.
+//  Copyright © 2020 Stone Zhu. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class GameViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
