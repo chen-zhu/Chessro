b957f18b69f9049526531433e22a53b30956bd97
Chen Z <chenzhu@ucsb.edu>
2020-05-20 16:11:16 -0700

    Testing structure & load sample entities

--- Chessro/GameViewController.swift
+++ Chessro/GameViewController.swift
@@ -14,13 +14,38 @@ class GameViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
+    var chessBoard = ChessBoard()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let boxAnchor = try! Experience.loadChessScene()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)//. 
+        arView.scene.anchors.append(boxAnchor)//.
+        //print("Chessboard!", chessBoard.ChessBoard)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.PeopleOcclusion()
+    }
+    
+    func PeopleOcclusion() {
+        guard let config = arView.session.configuration as? ARWorldTrackingConfiguration else {
+            fatalError("Unexpectedly failed to get the configuration.")
+        }
+        guard ARWorldTrackingConfiguration.supportsFrameSemantics(.personSegmentationWithDepth) else {
+            fatalError("People occlusion is not supported on this device.")
+        }
+        switch config.frameSemantics {
+        case [.personSegmentationWithDepth]:
+            config.frameSemantics.remove(.personSegmentationWithDepth)
+        default:
+            config.frameSemantics.insert(.personSegmentationWithDepth)
+        }
+        arView.session.run(config)
+    }
+    
 }
--- Chessro/model/Pieces/Pawn.swift
+++ Chessro/model/Pieces/Pawn.swift
@@ -22,6 +22,8 @@ class Pawn: ChessPiece{
     
     var ARObject: Entity
     
+    var init_pos = true
+    
     init(setColor: color, r_pos: Int, c_pos: Int) {
         self.PieceColor = setColor
         self.type = .pawn
@@ -34,7 +36,42 @@ class Pawn: ChessPiece{
     func validStep(chessBoard: ChessBoard) -> Array<SIMD2<Int>>{
         var result: Array<SIMD2<Int>> = []
         
+        //White goes up && black piece goes downward!
+        let direction = PieceColor == .white ? 1 : -1
+        let checkRow = row + direction
+        let checkCol = column
         
+        //If the forawrd position is numm, then move!
+        if chessBoard.ChessBoard[checkRow][checkCol] == nil {
+            result.append([checkRow, checkCol])
+            if init_pos {
+                let moreStep = checkRow + direction
+                if chessBoard.ChessBoard[moreStep][checkCol] == nil {
+                    result.append([moreStep, checkCol])
+                }
+            }
+        }
+                
+        /**
+         TODO:  Need to handle En Passant case!
+         */
+        
+        //If enemy is in diagonal direction~
+        let checkLeft = checkCol - 1
+        if checkLeft >= 0 {
+            let piece = chessBoard.ChessBoard[checkRow][checkLeft]
+            if piece != nil && piece?.PieceColor != PieceColor {
+                result.append([checkRow, checkLeft])
+            }
+        }
+        
+        let checkRight = checkCol + 1
+        if checkRight <= 7 {
+            let piece = chessBoard.ChessBoard[checkRow][checkRight]
+            if piece != nil && piece?.PieceColor != PieceColor {
+                result.append([checkRow, checkRight])
+            }
+        }
         
         return result
     }
